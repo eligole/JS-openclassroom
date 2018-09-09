b16a33bc0f1122f6294924a3287226dd5cda6d14
eligole <ikaryon@gmail.com>
2018-09-09 11:54:13 +0200

    tp multiplication  chapitre 4

--- intro-javascript/Chapitre_4/js/multiplication.js
+++ intro-javascript/Chapitre_4/js/multiplication.js
@@ -1,22 +1,52 @@
-var table = Number(prompt("entre la table de multiplication"));
+// Verison A
+/*
+var table = "";
 
-while ((table <= 1) || (table >= 10)) {
-
+while ((table >= 1) || (table <= 10)) {
+    var table = Number(prompt("entre la table de multiplication"));
     if (table === 1) {
+        console.log
         console.log("1x0=");
     }
-
     if (table === 2) {
         console.log("2x0=");
     }
-
     if (table === 3) {
         console.log("3x0=");
     }
-
     if (table === 4) {
         console.log("4x0");
     }
+    if (table === 5) {
+        console.log("5x0=");
+    }
+    if (table === 6) {
+        console.log("6x0=");
+    }
+    if (table === 7) {
+        console.log("7x0=");
+    }
+    if (table === 8) {
+        console.log("8x0");
+    }
+    if (table === 9) {
+        console.log("9x0");
+    }
+    if (table === 10) {
+        console.log("10x0");
+    }
+}*/
+
+//Version B
+
+var table ="";
+
+while((table <= 2) || (table >=9)){
+var table = Number(prompt("entrer la table de multiplication"));
 
 }
 
+
+for (i=1 ; i<=10 ; table++){
+console.log(table+ "x" +i +"=" +table * i);
+}
