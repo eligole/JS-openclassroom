9a693ce3ce47f7e1088f08d70a54cc5d0da770a4
eligole <ikaryon@gmail.com>
2018-09-30 15:23:29 +0200

    FIN tp fizzbuzz

--- intro-javascript/Chapitre_4/js/fizzbuzz.js
+++ intro-javascript/Chapitre_4/js/fizzbuzz.js
@@ -1,14 +1,32 @@
+
+
 for (var i = 1; i <= 100; i++) {
     //console.log(e++);
-    if (i % 3) {
-        
-        console.log("buzz");
-    } else if (i % 5) {
-        console.log("fizz");
+    //if ((i % 3) || (i%3!=0)) {
+
+    var k= '';
+    var j='';
+        if (i % 3 == k) {
+        console.log(k+"buzz");
+
+
+    } else if (i % 5==j) {
+        console.log(j+"fizz");
 
     } else {
-
+        console.log(i);
     }
-    var e = 1;
-console.log(e++);
+    //var e = 1;
+    //console.log(e++);
+
 }
+
+
+/*
+console.log(  3 %  3 ); = 0
+
+console.log(  3 %  5 );= 3
+
+1 2 4 7 8 11 ...  = non
+*/
+
