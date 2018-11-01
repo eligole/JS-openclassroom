2d9a1c45cdd24d188799f9024453232e53c7ef34
eligole <ikaryon@gmail.com>
2018-11-01 12:25:13 +0100

    suite du cours sur chapitre 8

--- intro-javascript/Chapitre_8/js/jdr.js
+++ intro-javascript/Chapitre_8/js/jdr.js
@@ -1,26 +1,48 @@
 var Personnage = {
-    nom: "",
-    sante: 0,
-    force: 0,
-    xp: 0,
-
-    // Renvoie la description du personnage
-    decrire: function () {
-        var description = this.nom + " a " + this.sante + " points de vie, " +
-            this.force + " en force et " + this.xp + " points d'expérience";
-        return description;
+    // Initialise le personnage
+    initPerso: function (nom, sante, force) {
+        this.nom = nom;
+        this.sante = sante;
+        this.force = force;
     }
 };
 
-var perso1 = Object.create(Personnage);
-perso1.nom = "Aurora";
-perso1.sante = 150;
-perso1.force = 25;
+var Joueur = Object.create(Personnage);
+// Initialise le joueur
+Joueur.initJoueur = function (nom, sante, force) {
+    this.initPerso(nom, sante, force);
+    this.xp = 0; // L'expérience du joueur est toujours initialisée à 0
+};
+// Renvoie la description du joueur
+Joueur.decrire = function () {
+    var description = this.nom + " a " + this.sante + " points de vie, " +
+        this.force + " en force et " + this.xp + " points d'expérience";
+    return description;
+};
 
-var perso2 = Object.create(Personnage);
-perso2.nom = "Glacius";
-perso2.sante = 130;
-perso2.force = 35;
+var Adversaire = Object.create(Personnage);
+// Initialise l'adversaire
+Adversaire.initAdversaire = function (nom, sante, force, race, valeur) {
+    this.initPerso(nom, sante, force);
+    this.race = race;
+    this.valeur = valeur;
+};
 
-console.log(perso1.decrire());
-console.log(perso2.decrire());
+// ...
+
+// ...
+
+var joueur1 = Object.create(Joueur);
+joueur1.initJoueur("Aurora", 150, 25);
+
+var joueur2 = Object.create(Joueur);
+joueur2.initJoueur("Glacius", 130, 30);
+
+console.log("Bienvenue dans ce jeu d'aventure ! Voici nos courageux héros :");
+console.log(joueur1.decrire());
+console.log(joueur2.decrire());
+
+var monstre = Object.create(Adversaire);
+monstre.initAdversaire("ZogZog", 40, 20, "orc", 10);
+
+console.log("Un affreux monstre arrive : c'est un " + monstre.race + " nommé " + monstre.nom);
