cb50727781e4e14a35627aff458d35c463445568
eligole <42616367+eligole@users.noreply.github.com>
2018-11-01 18:10:44 +0100

    chapitre_8 , tp1

--- intro-javascript/Chapitre_8/js/jdr.js
+++ intro-javascript/Chapitre_8/js/jdr.js
@@ -4,6 +4,22 @@ var Personnage = {
         this.nom = nom;
         this.sante = sante;
         this.force = force;
+    },
+    // Attaque un personnage cible
+    attaquer: function (cible) {
+        if (this.sante > 0) {
+            var degats = this.force;
+            console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
+            cible.sante = cible.sante - degats;
+            if (cible.sante > 0) {
+                console.log(cible.nom + " a encore " + cible.sante + " points de vie");
+            } else {
+                cible.sante = 0;
+                console.log(cible.nom + " est mort !");
+            }
+        } else {
+            console.log(this.nom + " ne peut pas attaquer : il est mort...");
+        }
     }
 };
 
@@ -11,13 +27,22 @@ var Joueur = Object.create(Personnage);
 // Initialise le joueur
 Joueur.initJoueur = function (nom, sante, force) {
     this.initPerso(nom, sante, force);
-    this.xp = 0; // L'expérience du joueur est toujours initialisée à 0
+    this.xp = 0;
 };
 // Renvoie la description du joueur
 Joueur.decrire = function () {
     var description = this.nom + " a " + this.sante + " points de vie, " +
         this.force + " en force et " + this.xp + " points d'expérience";
     return description;
+};
+// Combat un adversaire
+Joueur.combattre = function (adversaire) {
+    this.attaquer(adversaire);
+    if (adversaire.sante === 0) {
+        console.log(this.nom + " a tué " + adversaire.nom + " et gagne " +
+            adversaire.valeur + " points d'expérience");
+        this.xp += adversaire.valeur;
+    }
 };
 
 var Adversaire = Object.create(Personnage);
@@ -28,6 +53,7 @@ Adversaire.initAdversaire = function (nom, sante, force, race, valeur) {
     this.valeur = valeur;
 };
 
+// ...
 // ...
 
 // ...
@@ -46,3 +72,12 @@ var monstre = Object.create(Adversaire);
 monstre.initAdversaire("ZogZog", 40, 20, "orc", 10);
 
 console.log("Un affreux monstre arrive : c'est un " + monstre.race + " nommé " + monstre.nom);
+
+monstre.attaquer(joueur1);
+monstre.attaquer(joueur2);
+
+joueur1.combattre(monstre);
+joueur2.combattre(monstre);
+
+console.log(joueur1.decrire());
+console.log(joueur2.decrire());
