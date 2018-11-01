e0c6c938552c72c3e2ababee4417b16b851f015e
eligole <ikaryon@gmail.com>
2018-11-01 19:21:29 +0100

    chapitre_8: tp chien fini

--- intro-javascript/Chapitre_8/js/chien.js
+++ intro-javascript/Chapitre_8/js/chien.js
@@ -1,5 +1,18 @@
 // TODO : ajoutez ici la définition de l'objet Chien
-    
+
+var Chien = {
+
+    init: function (nom, race, taille) {
+        this.nom = nom;
+        this.race = race;
+        this.taille = taille;
+    },
+
+    aboyer : function (){
+        return "Grrr! Grrr!";
+    }
+};
+
 var crokdur = Object.create(Chien);
 crokdur.init("Crokdur", "mâtin de Naples", 75);
 console.log(crokdur.nom + " est un " + crokdur.race + " mesurant " + crokdur.taille + " cm");
@@ -9,3 +22,46 @@ var pupuce = Object.create(Chien);
 pupuce.init("Pupuce", "bichon", 22);
 console.log(pupuce.nom + " est un " + pupuce.race + " mesurant " + pupuce.taille + " cm");
 console.log("Tiens, un chat ! " + pupuce.nom + " aboie : " + pupuce.aboyer());
+
+
+
+
+
+////Correction////////////////////////////////////////////////////////////////////////////////
+
+/*
+/*
+Exercice : objets chiens
+
+
+var Chien = {
+    // initialise le chien
+    init: function (nom, race, taille) {
+        this.nom = nom;
+        this.race = race;
+        this.taille = taille;
+    },
+    // Renvoie l'aboiement du chien
+    aboyer: function () {
+        var aboiement = "Whoua ! Whoua !";
+        if (this.taille < 25) {
+            aboiement = "Kaii ! Kaii !";
+        } else if (this.taille > 60) {
+            aboiement = "Grrr ! Grrr !";
+        }
+        return aboiement;
+    }
+};
+
+var crokdur = Object.create(Chien);
+crokdur.init("Crokdur", "mâtin de Naples", 75);
+console.log(crokdur.nom + " est un " + crokdur.race + " mesurant " + crokdur.taille + " cm");
+console.log("Tiens, un chat ! " + crokdur.nom + " aboie : " + crokdur.aboyer());
+
+var pupuce = Object.create(Chien);
+pupuce.init("Pupuce", "bichon", 22);
+console.log(pupuce.nom + " est un " + pupuce.race + " mesurant " + pupuce.taille + " cm");
+console.log("Tiens, un chat ! " + pupuce.nom + " aboie : " + pupuce.aboyer());
+
+
+*/
